dc8f97e1466f1c43d48e0b4f1f96630353528ec7
vjadhikari120 <vjadhikari120@gmail.com>
2026-04-08 20:15:31 -0500

    Modify CSV URL and basemap in createPhotoMap function
    Updated the CSV URL and basemap tile layer for the Leaflet map.

--- photomap.js
+++ photomap.js
@@ -14,16 +14,17 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuESp280YE8_PokNYJSK_V3zxGh7ZJlDqMNeliCoz1kt6q2ii7lRovZ-yMlEFY2n6rhUu3vOpnvFGx/pub?gid=0&single=true&output=csv';
+  var csvUrl = https://docs.google.com/spreadsheets/d/e/2PACX-1vRhSF1fOZp-FvWBScEDHQXzF6MpajJp6JIrKeVpqeYT__onGdBsZ-XsFqBf9pxq5_DkMF69yUYaMOlk/pub?gid=0&single=true&output=csv;
   
   // create map object with center lat/lon and zoom level
-  var map = L.map('map').setView([30.44, -91.187], 13);
+  var map = L.map('map').setView([30.433, -91.1833], 13);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-  	maxZoom: 16,
-  	attribution: 'USGS'
-  }).addTo(map);
+  var basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '© OpenStreetMap contributors © CARTO',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
   Papa.parse(csvUrl, {
